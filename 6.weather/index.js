613cd0c804f727f9fe88c620a7a766657a8075af
ladantork <ladantorkzaban0@gmail.com>
2022-08-29 09:06:47 +0100

    weather api

---
+++ 6.weather/index.js
@@ -0,0 +1,29 @@
+// const got = require('got');
+
+// const handler = (response) =>{
+//   const responseObject = JSON.parse(response.body);
+//   console.log(responseObject)
+// }
+
+// const url = 'https://api.github.com/repos/sinatra/sinatra';
+
+// got(url).then(handler);
+
+const got = require('got');
+
+const apiKey = 'b482ccbeddccd08e2c756c46bf18761c'; // include our key
+const city = 'London';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+
+let weatherData = null;
+
+got(apiUrl).then((response) => {
+  weatherData = JSON.parse(response.body);
+  console.log(weatherData);
+  console.log(weatherData.main.temp);
+  console.log(weatherData.weather[0].main);
+});
+
+
+
+console.log('Requesting weather data');
